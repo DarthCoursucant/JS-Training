da171c1e3c681af2ca59a5ea6150002e1469f93f
DarthCoursucant <Ameliakingabujar2002@gmail.com>
2024-01-27 17:20:25 +0100

    Create script.js

---
+++ Task-2/script.js
@@ -0,0 +1,8 @@
+console.log("script.js is connected");
+
+//Exersice 1
+var firstName = "Amelia"
+var middleName = "Kinga"
+var lastName = "Bujar"
+
+console.log(firstName + " " + middleName + " " + lastName)
