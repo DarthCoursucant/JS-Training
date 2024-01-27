b3fa3a53680a6c783243fbc9cd6f2530a193dc08
DarthCoursucant <Ameliakingabujar2002@gmail.com>
2024-01-27 15:56:40 +0100

    Update script.js

--- Exericse One/script.js
+++ Exericse One/script.js
@@ -7,3 +7,8 @@ console.log("Exercise One Shit")
 // Files Out The blankParagraph ID from index 
 const p = document.getElementById("blankParagraph");
 p.textContent = "DarthCouruscant"
+
+// Exerice 1.3
+const ul = document.getElementById("emptyList");
+ul.innerHTML = "<li>HTML</li>";
+ul.innerHTML += "<li>CSS</li>";
