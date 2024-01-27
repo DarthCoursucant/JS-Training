8975a9892843056b6adf07158a10ad66ef53a220
DarthCoursucant <Ameliakingabujar2002@gmail.com>
2024-01-27 17:20:43 +0100

    Update script.js

--- Task-2/script.js
+++ Task-2/script.js
@@ -1,8 +1,10 @@
 console.log("script.js is connected");
 
-//Exersice 1
+//Exersice 2.1
 var firstName = "Amelia"
 var middleName = "Kinga"
 var lastName = "Bujar"
 
 console.log(firstName + " " + middleName + " " + lastName)
+
+// Exericse 2.2
