eb4de5ca5f71807d38005218db023aa8dbe7a96f
DarthCoursucant <Ameliakingabujar2002@gmail.com>
2024-01-27 17:25:42 +0100

    Update script.js

--- Task-2/script.js
+++ Task-2/script.js
@@ -12,3 +12,9 @@ var x = 19;
 var y = 23;
 var answer = x + y;
 console.log(answer)
+
+// Exerice 2.3
+// When you dont assign any value to varaible the console displays "undefiend"
+var myBool = "true"
+console.log(myBool)
+// console.log(!myBool); //False
