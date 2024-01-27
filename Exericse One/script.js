e3d38294d7a2d845bf7c23e7e348cee92d2d16dc
DarthCoursucant <Ameliakingabujar2002@gmail.com>
2024-01-27 14:45:33 +0100

    Update script.js

--- Exericse One/script.js
+++ Exericse One/script.js
@@ -3,3 +3,6 @@ console.log("Hello, world from script.js");
 
 console.log("Exercise One Shit")
 
+// Exericse 1.2
+const p = document.getElementById("blankParagraph");
+p.textContent = "Hello From ...?"
