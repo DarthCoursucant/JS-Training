cd1b24f02c66a2489f086fd3f125c1bf7dd5b0bb
DarthCoursucant <Ameliakingabujar2002@gmail.com>
2024-01-27 17:22:33 +0100

    Update script.js

--- Task-2/script.js
+++ Task-2/script.js
@@ -1,10 +1,14 @@
 console.log("script.js is connected");
 
 //Exersice 2.1
-var firstName = "Amelia"
-var middleName = "Kinga"
-var lastName = "Bujar"
+var firstName = "Amelia";
+var middleName = "Kinga";
+var lastName = "Bujar";
 
 console.log(firstName + " " + middleName + " " + lastName)
 
 // Exericse 2.2
+var x = 19;
+var y = 23;
+var sum = x + y
+console.log(sum)
