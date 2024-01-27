2e1cb85c6d66cbdc9c941a100b714528120fae9f
DarthCoursucant <Ameliakingabujar2002@gmail.com>
2024-01-27 20:03:28 +0100

    Update script.js

--- Task-3/script.js
+++ Task-3/script.js
@@ -71,3 +71,88 @@ if (isNaN(test)) {
 }
 
 // Tip: Check the input using [the Number() function](https://www.w3schools.com/jsref/jsref_number.asp) and [the isNaN() function](https://www.w3schools.com/jsref/jsref_isnan.asp).
+
+// Exxcerise 3.6 
+var number = prompt("Please, enter a number");
+if (number % 2 == 0) {
+    console.log (number + " is an even number");
+} else {
+    console.log (number + " is an odd number");
+}
+
+// Excerise 3.7
+
+let inputName1 = prompt("What is your name?");
+if (inputName1 === "" || inputName1 === null) {
+    alert ("Awww!");
+} else {
+    alert ("Hello, " + inputName1);
+}
+// NOTE: according to documentation, prompt() should return null when cancel is pressed
+// Beware that if inputName is decleard with var, and not let, then null is returned as a string
+
+// Excerise 3.8 a + b + c
+
+var fruit = "kiwi";
+var output;
+switch (fruit) {
+    case "apple":
+        answerfruit = "An apple a day...";
+        break;
+    case "orange":
+        answerfruit = "What to do with all the orange peel?";
+        break;
+    case "pear":
+        answerfruit = "Delicious, from a tree or a can.";
+        break;
+    case "banana":
+        answerfruit = "What, are you a Minion?"
+        break;
+    case "mango":
+        answerfruit = "Mangos are weird...";
+        break;
+    default:
+        answerfruit = "That's not a fruit, at least not one I've heard of.";
+}
+console.log(answerfruit);
+
+// Excerise 3.9 a + b + c
+let month = prompt("Enter a month: ");
+month = month.toLowerCase();
+switch (month) {
+    case "december":
+    case "january":
+    case "february":
+        console.log("It's Winter!"); break;
+    case "march":
+    case "april":
+    case "may":
+        console.log("It's Spring!"); break;
+    case "june":
+    case "july":
+    case "august":
+        console.log("It's Summer!"); break;
+    case "september":
+    case "october":
+    case "november":
+        console.log("It's Autumn!"); break;
+    default:
+}
+console.log("That's not a month...");
+
+// Excerise 3.10
+let chosenVoltage = prompt("What voltage does your appliance need?");
+chosenVoltage = parseInt(chosenVoltage); // Convert mixed answer to number
+if (isNaN(chosenVoltage)) {
+    // chosenVoltage still isn't a number after conversion
+    alert("You need to input a number");
+} else {
+    // chosenVoltage is a number after conversion
+    if (chosenVoltage >= 220 && chosenVoltage <= 240) {
+        // chosenVoltage is between 220 and 240
+        alert ("This can run on the usual grid");
+    } else {
+        // chosenVoltage is NOT between 220 and 240
+        alert ("This need an inverter or something");
+    }
+}
