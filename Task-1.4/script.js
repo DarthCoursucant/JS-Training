9a7e362f9566ef0003e6fcb15101b77ec183a2b3
DarthCoursucant <Ameliakingabujar2002@gmail.com>
2024-01-28 13:33:10 +0100

    Create script.js

---
+++ Task-1.4/script.js
@@ -0,0 +1,16 @@
+console.log("You're a mean one, mr. Grinch!");
+
+//Excerise 1.4.1 a
+for (let i = 0; i < 11; i++) {
+    console.log(i);
+}
+
+//Excerise 1.4.1 b
+for (let i = 6; i < 12; i++) {
+    console.log(i);
+}
+
+// Excerise 1.4.1 c
+for (let i = 10; i > 0; i-- ) {
+    console.log(i);
+}
