2541ca68dc3918121602fad88e021b596d226ce7
DarthCoursucant <Ameliakingabujar2002@gmail.com>
2024-01-28 13:24:44 +0100

    Create script.js

---
+++ Task-1-2/script.js
@@ -0,0 +1,41 @@
+console.log("script.js is connected");
+
+//Exersice 1.2.1
+var firstName = "Amelia";
+var middleName = "Kinga";
+var lastName = "Bujar";
+
+console.log(firstName + " " + middleName + " " + lastName)
+
+// Exericse 1.2.2
+var x = 19;
+var y = 23;
+var answer = x + y;
+console.log(answer)
+
+// Exerice 1.2.3
+// When you dont assign any value to varaible the console displays "undefiend"
+var myBool = "true"
+console.log(myBool)
+// console.log(!myBool); //False
+
+// Exerivce 1.2.4
+var theText = "Blåbærsyltetøy";
+console.log(theText);
+
+// Exercise 1.2.5
+// the result is 1912 aka mashes the two values together with caluculating
+var a = "19";
+var b = 23;
+console.log(a + b);
+//This One makes it calculate the values together withiout chainging the varaibles
+console.log(Number(a) + b);
+
+// Exercise 1.2.6
+var x = 19, y = 23, z = 10;
+console.log(x + y + z); // 52
+console.log(x + y * z); //249
+console.log ((x + y) * z); //420
+
+// Exercise 1.2.7 
+console.log("Exercise 2.7" + " " + "The Result Is =" + " " + 7 % 3); //Exercise 2.7" 1
