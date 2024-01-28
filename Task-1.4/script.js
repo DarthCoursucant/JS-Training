b3fe2dcdd88812446c5be84b98e992daab768420
DarthCoursucant <Ameliakingabujar2002@gmail.com>
2024-01-28 15:04:41 +0100

    Update script.js

--- Task-1.4/script.js
+++ Task-1.4/script.js
@@ -14,3 +14,21 @@ for (let i = 6; i < 12; i++) {
 for (let i = 10; i > 0; i-- ) {
     console.log(i);
 }
+
+// Excerise 1.4.2 a
+let i = 0;
+while (i <= 10) {
+    console.log(i);
+i++; }
+
+// Excerise 1.4.2 b
+let b = 6;
+while (b <=11) {
+    console.log(b);
+b++}
+
+// Excerise 1.4.2 c
+let p = 10;
+while (p > 0) {
+    console.log(p)
+p--}
